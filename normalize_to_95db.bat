aba0e41518f9a859b71a5150a1d1a48947d2f404
scMarth <vlantaca@gmail.com>
2017-02-03 19:59:50 -0800

    allow for cross-folder drag and drop

--- normalize_to_95db.bat
+++ normalize_to_95db.bat
@@ -1,9 +1,18 @@
+set batchpath=%~dp0%
+
 :loop
 
 @echo off
 
-
+set fullpath=%1
 set filename="%~nx1%"
+set folder=%~dp1%
+
+echo fullpath = %fullpath%
+echo filename = %filename%
+echo folder = %folder%
+echo batchpath = %batchpath%
+
 
 rename %filename% temporary_filename_500.mp3
 
@@ -11,8 +20,9 @@ REM -c : ignore clipping
 REM -r : apply track gain
 REM -d 6.0 : makes it 95.0 dB (default to 89.0 dB)
 
-mp3gain -c -r -d 6.0 temporary_filename_500.mp3
-rename temporary_filename_500.mp3 %filename%
+%batchpath%mp3gain -c -r -d 6.0 %folder%temporary_filename_500.mp3
+
+rename %folder%temporary_filename_500.mp3 %filename%
 
 shift
 if not "%~1"=="" goto loop
