445ff720ff04936e12b81604d92ed8386d5b9f82
scMarth <vlantaca@gmail.com>
2017-02-24 20:34:16 -0800

    fixed bug that occured when there was a space in paths

--- normalize_to_95db.bat
+++ normalize_to_95db.bat
@@ -13,16 +13,13 @@ echo filename = %filename%
 echo folder = %folder%
 echo batchpath = %batchpath%
 
-
-rename %filename% temporary_filename_500.mp3
-
+rename %filename% "temporary_filename_500.mp3"
 REM -c : ignore clipping
 REM -r : apply track gain
 REM -d 6.0 : makes it 95.0 dB (default to 89.0 dB)
+call "%batchpath%mp3gain" -c -r -d 6.0 "%folder%temporary_filename_500.mp3"
 
-%batchpath%mp3gain -c -r -d 6.0 %folder%temporary_filename_500.mp3
-
-rename %folder%temporary_filename_500.mp3 %filename%
+rename "%folder%temporary_filename_500.mp3" %filename%
 
 shift
 if not "%~1"=="" goto loop
